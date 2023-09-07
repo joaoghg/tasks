8290a0f7cd0a3198791cfcedc18e9a7b85164d74
joaoghg <jherreiragarnica@gmail.com>
2023-09-07 16:17:14 -0300

    tasks

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -1,3 +1,15 @@
 module.exports = app => {
     app.post('/signup', app.api.user.save)
+    app.post('/signin', app.api.auth.signIn)
+
+    app.route('/tasks')
+        .all(app.config.passport.authenticate())
+        .get(app.api.task.getTasks)
+        .post(app.api.task.save)
+
+    app.route('/tasks/:id')
+        .all(app.config.passport.authenticate())
+        .delete(app.api.task.remove)
+        .put(app.api.task.toggleTask)
+
 }
--- backend/index.js
+++ backend/index.js
@@ -3,13 +3,15 @@ const app = express()
 const db = require('./config/db')
 const consign = require('consign')
 
+app.db = db
+
 consign()
+    .include('./config/passport.js')
     .then('./config/middlewares.js')
     .then('./api')
     .then('./config/routes.js')
     .into(app)
 
-app.db = db
 
 app.listen(3000, () => {
     console.log("backend executando")
